c02a1c59b50d889f0597c4164258dffca35bd6b1
rojogetmojo <guyguyjoe122@gmail.com>
2021-06-18 23:45:27 +0800

    node-2

--- myApp.js
+++ myApp.js
@@ -3,4 +3,8 @@ var app = express();
 
 console.log("Hello World");
 
+app.get("/",(req , res )=>{
+    res.send("Hello Express");
+})
+
  module.exports = app;
--- server.js
+++ server.js
@@ -16,6 +16,10 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
+app.get("/",(req , res )=>{
+  res.send("Hello Express");
+})
+
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
   bGround.log('Node is listening on port '+ port + '...')
